991b94dafbfb57830178b1b1b2d4cb62ff428a9c
gordonb3 <gordon@bosvangennip.nl>
2016-07-14 09:43:54 +0200

    fix bad interpretation of nmcli output

--- systemd/sbin/bubba-restore-defaults.sh
+++ systemd/sbin/bubba-restore-defaults.sh
@@ -144,16 +144,16 @@ if $(ip link show wlan0 2>/dev/null | grep -q "state"); then
 	systemctl is-enabled hostapd &>/dev/null && systemctl disable hostapd
 fi
 if $(systemctl is-active NetworkManager &>/dev/null); then
-	conn=$(nmcli d | grep "^eth0\s")
-	if [ conn != "WAN" ]; then
-		if [ conn != "--" ]; then
+	conn=$(nmcli d | grep "^eth0\s" | awk '{print $NF}')
+	if [ "conn" != "WAN" ]; then
+		if [ "conn" != "--" ]; then
 			nmcli connection delete ${conn}
 		fi
 		nmcli connection add type ethernet con-name WAN ifname eth0
 	fi
-	conn=$(nmcli d | grep "^eth1\s")
-	if [ conn != "LAN" ]; then
-		if [ conn != "--" ]; then
+	conn=$(nmcli d | grep "^eth1\s" | awk '{print $NF}')
+	if [ "conn" != "LAN" ]; then
+		if [ "conn" != "--" ]; then
 			nmcli connection delete ${conn}
 		fi
 		nmcli connection add type ethernet con-name LAN ifname eth1
